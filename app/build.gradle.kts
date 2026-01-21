8afa86a8965190f0bf18ce31f41c16a72dc32f0f
ofirShriki <ofir3224@rogozin.net>
2026-01-21 18:14:59 +0200

    fixed viewbinding bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("androidx.databinding:viewbinding:8.13.2") // Use direct coordinate
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
